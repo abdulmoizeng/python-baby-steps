82bc9623b27635e0445c6bc58f1e4135a083997c
abdulmoiz <abdulmoiz@marketlytics.com>
2018-08-24 15:01:46 +0500

    chore: Keyword argument unpacking syntax example added

--- step4/index.py
+++ step4/index.py
@@ -20,6 +20,19 @@ def sub_number(num1, num2):
 result = sub_number(10, 15)
 print(result)
 
+def sum(num1, num2, num3):
+    return num1 + num2 + num3
+
+args = (1, 2, 3)  # usually a tuple, always an iterable[1]
+# The *args argument is called the "variable positional parameter"
+print(sum(*args))   # -> sum(1, 2, 3)
+
+# **kwargs is the "variable keyword parameter"
+
+kwargs = {"num1": 1, "num2": 2, "num3": 3}  # usually a dict, always a mapping*
+
+print(sum(**kwargs)) # -> sum(1, 2, 3)
+
 # *****************                     ***********************
 
 # ********** Taking cli based input from user  **************
